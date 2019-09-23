c04ea8081c13f73932a5a0442c8e986438ec2685
Andreas <roflcoffel@gmail.com>
2019-09-23 05:24:32 +0200

    Unit Test, And fixing util

--- util.c
+++ util.c
@@ -1,24 +1,29 @@
 //Hopefully useful functions
 #include "util.h"
+#include <string.h>
+#include <stdlib.h>
 
-char * substring(int start, int end, char string[MAX_SIZE])
-{
-        char *substring[MAX_SIZE];
-        int tmp;
-
+//Creates a substring from a string adds a null character
+//start       - array start index
+//num_of_char - number of total characters, 0 == length of "string";
+//string      - string to extract from
+char * substring(int start, int num_of_char, char string[MAX_SIZE])
+{       
         if(start > MAX_SIZE) return string;
-        if(end > MAX_SIZE) end = MAX_SIZE;
-        if(start > end)
-        {
-                tmp = start;
-                start = end;
-                end = tmp;        
-        }
 
-        for(int i = start; i < end; i++)
+        num_of_char += start;
+
+        if(num_of_char == 0)       num_of_char = strlen(string);
+        if(num_of_char > MAX_SIZE) num_of_char = MAX_SIZE-1;
+
+        char * substring = malloc(MAX_SIZE * sizeof(char));
+
+        for(int i = start; i < num_of_char; i++)
         {
                 substring[i] = string[i];
         }
+
+        substring[num_of_char] = '\0';
         
         return substring;
 }
@@ -26,15 +31,21 @@ char * substring(int start, int end, char string[MAX_SIZE])
 //Returns an array with two strings
 char ** str_split(char string[MAX_SIZE], char delimiter)
 {
-        char str[2][MAX_SIZE];
+        char ** str = malloc(2 * sizeof(char*));
         int len = strlen(string);
-        strcpy(str[0], string);
+
+        str[0] = malloc(MAX_SIZE * sizeof(char));
+        str[1] = malloc(MAX_SIZE * sizeof(char));
 
         for(int i = 0; i < len; i++)
         {
-                if(string[i] == delimiter) {
-                        strcpy(str[0], substring(0,i-1,string));
-                        strcpy(str[1], substring(i+1,len,string));
+                if(string[i] == delimiter) 
+                {
+                        char s0[MAX_SIZE] = substring(0,i-1,string);
+                        char s1[MAX_SIZE] = substring(i+1,0,string);
+                        strcpy(str[0], s0);
+                        strcpy(str[1], s1);
+                        free(s0); free(s1);
                         break;
                 }
         }
--- util.h
+++ util.h
@@ -1,7 +1,7 @@
 #ifndef UTIL_H_
 #define UTIL_H_
 
-#define MAX_SIZE 1000
+#define MAX_SIZE 255
 
 char * substring(int start, int end, char string[MAX_SIZE]);
 char ** str_split(char string[MAX_SIZE], char delimiter);
